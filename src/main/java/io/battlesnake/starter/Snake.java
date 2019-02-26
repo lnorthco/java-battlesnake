4eab34fa92d02316c5021fd1b56d303f6bd292ff
Logan Northcote <l.a.northcote@gmail.com>
2019-02-26 00:11:37 -0800

    Getting myHead still

--- src/main/java/io/battlesnake/starter/Snake.java
+++ src/main/java/io/battlesnake/starter/Snake.java
@@ -123,7 +123,7 @@ public class Snake {
 			//Get game variables
 			int height = moveRequest.get("board").get("height");
 			int width = moveRequest.get("board").get("height");
-			int myHead = moveRequest.get("you").get("body").toMap().values().iterator().next();
+			String myHead = moveRequest.get("you").get("body").get(0);
 			//System.out.println(moveRequest.get("board").get("height"));
 			System.out.println(myHead);
 			
