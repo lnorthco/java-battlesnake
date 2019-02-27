955b4bfb0548301489c4fd54a7eb22fb912db955
Logan Northcote <l.a.northcote@gmail.com>
2019-02-26 21:40:06 -0800

    Adding snake detection.

--- src/main/java/io/battlesnake/starter/Snake.java
+++ src/main/java/io/battlesnake/starter/Snake.java
@@ -151,7 +151,9 @@ public class Snake {
 			for(int j=0; j<numOfSnakes; j++) {
 				snakeBody = snakes.get(j).get("body");
 				System.out.println("snakeBody="+snakeBody);
+				System.out.println("snakeSize="+snakeBody.size());
 				for(int i=0; i<snakeBody.size(); i++){
+					System.out.println("test");
 					System.out.println("x:" + xHead + "=" + snakeBody.get("x").intValue());
 					System.out.println("y:" + yHead + "=" + snakeBody.get("y").intValue());
 					if(snakeBody.get("x").intValue() == xHead && snakeBody.get("y").intValue() == yHead-1)up=false;
