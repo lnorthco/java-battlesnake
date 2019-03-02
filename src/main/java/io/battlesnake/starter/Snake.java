0b8471037a6fdcecd3285766e2f2d31a0fd633a0
Taylor <taylorkmac16@gmail.com>
2019-03-02 10:24:05 -0800

    Update Snake.java

--- src/main/java/io/battlesnake/starter/Snake.java
+++ src/main/java/io/battlesnake/starter/Snake.java
@@ -114,6 +114,8 @@ public class Snake {
         public Map<String, String> start(JsonNode startRequest) {
             Map<String, String> response = new HashMap<>();
             response.put("color", "#ff00ff");
+            response.put("headType", "beluga");
+            response.put("tailType", "curled");
             return response;
         }
 
